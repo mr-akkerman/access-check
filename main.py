b42c2c189adc95bfdb44f934c9d4762035332840
ivan.akkerman <akkerman.live@gmail.com>
2023-10-01 21:45:20 -0300

    🤝 New method (POST) for checking availability ‼️ New structure of the Paths API: the current path has been renamed to access-checking. The current verification service has been renamed to simple-check. A simple check allows you to get simple data, just as before: Status + Timestamp

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ logging.basicConfig(
     format="u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s",
     filename='logs/logs.log',
 )
-app = FastAPI(title='Access-Check by Akkerman', version='0.0.1')
+app = FastAPI(title='Access-Check by Akkerman', version='1.0.0')
 
 # controller register
-app.include_router(standard_check_controller.router, prefix='/v1/standard-check', tags=['🔐 Access check'])
+app.include_router(simple_access_check_routes.router, prefix='/v1/access-checking', tags=['🔐 Access check'])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
